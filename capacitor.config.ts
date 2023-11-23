1312f719d747ca54e3e02d4f1fd7772841747449
lugraff <lugraff@gmx.net>
2023-11-23 21:16:47 +0100

    android prepared

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.lugraff.webknot',
+  appName: 'WebKnot',
+  webDir: 'docs',
+  server: {
+    androidScheme: 'https',
+  },
+};
+
+export default config;
