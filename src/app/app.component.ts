fd5cde66e57095720f85b3a5e136c8d1c6054a60
lugraff <lugraff@gmx.net>
2023-11-23 22:06:20 +0100

    Statusbar Color settet and BackBehavior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, RendererFactory2, inject } from '@angular/core';
+import { App as CapacitorApp } from '@capacitor/app';
+import { CommonModule, DOCUMENT } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { Capacitor } from '@capacitor/core';
+import { StatusBar, Style } from '@capacitor/status-bar';
+import { NavigationBar } from '@hugotomazi/capacitor-navigation-bar';
 
 @Component({
   selector: 'app-root',
@@ -13,4 +17,28 @@ import { RouterModule } from '@angular/router';
     </div>
   `,
 })
-export class AppComponent {}
+export class AppComponent {
+  private _renderer = inject(RendererFactory2).createRenderer(null, null);
+  private _document = inject(DOCUMENT);
+  constructor() {
+    if (Capacitor.isNativePlatform()) {
+      CapacitorApp.addListener('backButton', ({ canGoBack }) => {
+        if (!canGoBack) {
+          CapacitorApp.exitApp();
+        } else {
+          window.history.back();
+        }
+      });
+    }
+    if (Capacitor.isNativePlatform()) {
+      const element = this._document.getElementById('footerPanel');
+      let footerColor = '#ffffff';
+      if (element) {
+        footerColor = getComputedStyle(element).backgroundColor;
+      }
+      NavigationBar.setColor({ color: '#00000000', darkButtons: false });
+      StatusBar.setStyle({ style: Style.Dark });
+      StatusBar.setBackgroundColor({ color: '#00000000' });
+    }
+  }
+}
